cd4334f04cc3db6ff77a8a571b3210dc22ae00aa
Andrea Gil <andreagilruiz@gmail.com>
2019-05-15 21:36:11 +0200

    Created a class Explorer

--- src/avoid_obstacles.py
+++ src/avoid_obstacles.py
@@ -9,61 +9,76 @@ from nav_msgs.msg import Odometry
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 import matplotlib.pyplot as plt
 
+class Explorer:
+    def __init__(self): #Class constructor
+        rospy.init_node('obstacle_avoidance')  # Initiate a Node called 'obstacle_avoidance'
+        laser_sub = rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)  # Subscribe to /scan topic to obtain laser info
+        odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_callback)  # Subscribe to /odom topic to obtain laser info
+        map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)  # Subscribe to /map topic to obtain laser info
 
-#Define a function called 'callback' that receives a parameter named 'msg'
-def callback(laserscan, odometry):
-    #global order
-    print('==========================================')
-    print('s1 [270]') #value right-direction laser beam
-    print laserscan.ranges[270]
-    print('s2 [0]') #value front-direction laser beam
-    print laserscan.ranges[0] #print the distance to an obstacle in front of the robot
-    #The sensor returns a vector of 359 values being the initial value the corresponding to the front of the robot
-    print('s3 [90]') #value left-direction laser beam
-    print laserscan.ranges[90]
-    print('Pose:')
-    print odometry.pose.pose
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)  # Create a publisher on the /cmd_vel topic
+
+        self.move = Twist()
+        self.laser = None
+        self.odom = None
+        self.map = None
+        # actual drive state (initial: WallDetection)
+        self.driveState = "WallDetection"
+
+    #Function to read'nav_msgs/OccupancyGrid' messages and turn them into numpy arrays
+    # from https://github.com/eric-wieser/ros_numpy
+    def occupancygrid_to_numpy(self):
+        data = numpy.asarray(self.map.data, dtype=numpy.int8).reshape(self.map.info.height, self.map.info.width)
+
+        return numpy.ma.array(data, mask=data==-1, fill_value=-1)
+
+    #Define a function called 'callback' for receiving parameters named 'msg'
+    def laserscan_callback(self, laser_msg):
+        self.laser = laser_msg
+
+    def odom_callback(self, odom_msg):
+        self.odom = odom_msg
+
+    def map_callback(self, map_msg):
+        self.map = map_msg
+
+    def startExplorer(self):
+        while not rospy.is_shutdown():
+            if (self.laser and self.odom):  # laser and odom data arrived from callback
+                print('==========================================')
+                print('s1 [270]') #value right-direction laser beam
+                print self.laser.ranges[270]
+                print('s2 [0]') #value front-direction laser beam
+                print self.laser.ranges[0] #print the distance to an obstacle in front of the robot
+                #The sensor returns a vector of 359 values being the initial value the corresponding to the front of the robot
+                print('s3 [90]') #value left-direction laser beam
+                print self.laser.ranges[90]
+                print('Pose:')
+                print self.odom.pose.pose
+
+                # If the distance to an obstacle in front of the robot is bigger than 1 meter, the robot will move forward
+                if self.laser.ranges[0] > 0.5:
+                    self.move.linear.x = 0.22
+                    self.move.angular.z = 0
+                else:
+                    self.move.linear.x = 0
+                    self.move.angular.z = 1
+
+                self.pub.publish(self.move)  # Publish movement
+
+            if (self.map):  # occupancy data arrived from callback
+                print('Map:')
+                map=self.occupancygrid_to_numpy() #Occupancy map using numpy format
+                #Plot occupancy map
+                plt.imshow(map)
+                plt.draw()
+                plt.pause(0.001)
+                plt.hold()  # toggle hold
+                plt.hold(True)
 
 
-    #If the distance to an obstacle in front of the robot is bigger than 1 meter, the robot will move forward
-    if laserscan.ranges[0]>0.5:
-        move.linear.x=0.22
-        move.angular.z=0
-    else:
-        move.linear.x=0
-        move.angular.z=1
+        rospy.spin()
 
-    pub.publish(move) #Publish movement
-
-#Function to read'nav_msgs/OccupancyGrid' messages and turn them into numpy arrays
-# from https://github.com/eric-wieser/ros_numpy
-def occupancygrid_to_numpy(msg):
-	data = numpy.asarray(msg.data, dtype=numpy.int8).reshape(msg.info.height, msg.info.width)
-
-	return numpy.ma.array(data, mask=data==-1, fill_value=-1)
-
-#Callback function to read map from rsviz
-def callback2(occupancy):
-    print('Map:')
-    map = occupancygrid_to_numpy(occupancy)
-    plt.imshow(map)
-    plt.draw()
-    plt.pause(0.001)
-    plt.hold()  # toggle hold
-    plt.hold(True)
-
-rospy.init_node('obstacle_avoidance') #Initiate a Node called 'obstacle_avoidance'
-laser_sub = message_filters.Subscriber('/scan', LaserScan) #Subscribe to /scan topic to obtain laser info
-odom_sub = message_filters.Subscriber('/odom', Odometry) #Subscribe to /odom topic to obtain laser info
-map_sub = message_filters.Subscriber('/map', OccupancyGrid) #Subscribe to /map topic to obtain laser info
-
-ts = message_filters.ApproximateTimeSynchronizer([laser_sub, odom_sub], 1, 1)
-ts.registerCallback(callback)
-
-ts2 = message_filters.ApproximateTimeSynchronizer([map_sub], 1, 1)
-ts2.registerCallback(callback2)
-
-pub = rospy.Publisher('cmd_vel', Twist, queue_size=1) #Create a publisher on the /cmd_vel topic
-move=Twist()
-
-rospy.spin()
+if __name__ == "__main__":
+    explorer_obj = Explorer() #Create an object
+    explorer_obj.startExplorer() #Call the function to initiate exploration
